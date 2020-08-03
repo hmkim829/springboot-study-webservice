08d694d8e36b7f6703a2e58af5c09801f859b728
hmkim829 <hmkim829@gmail.com>
2020-08-04 01:48:44 +0900

    게시글 등록

---
+++ src/main/java/com/study/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.study.book.springboot.service.posts;
+
+import com.study.book.springboot.domain.posts.PostsRepository;
+import com.study.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
