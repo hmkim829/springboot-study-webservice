66e6e63973a65c73b8b29665dad5e33de47a6e59
hmkim829 <hmkim829@gmail.com>
2020-08-15 00:35:02 +0900

    update api 작성

--- src/main/java/com/study/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/study/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.study.book.springboot.service.posts;
 
 import com.study.book.springboot.domain.posts.Posts;
 import com.study.book.springboot.domain.posts.PostsRepository;
+import com.study.book.springboot.web.dto.PostsListResponseDto;
 import com.study.book.springboot.web.dto.PostsResponseDto;
 import com.study.book.springboot.web.dto.PostsSaveRequestDto;
 import com.study.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(()-> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDESC().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
